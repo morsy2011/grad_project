872af7b6f2b4b46411a6e04e66bad990e726da19
Mahmoud Morsy <mahmoudmorsy530@gmail.com>
2022-02-27 02:37:51 +0200

    final edit

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.post('/', async (req, res) =>{
   const { error } = validateUser(req.body);
   if(error) return res.status(404).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email});
+  let user = await User.findOne({ email: req.body.email}).exec();
   if(user) return res.status(400).send('User already registered.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
