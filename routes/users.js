e2b0c68508f6f326a17ec97dda1209c8c52c178d
Mahmoud Morsy <mahmoudmorsy530@gmail.com>
2022-02-27 15:39:51 +0200

    final edit

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,7 @@ router.post('/', async (req, res) =>{
 });
 
 router.get('/', async (req, res) => {
-  const user = await User.find().sort('name -password');
+  const user = await User.find().sort('name').select('-password');
   res.send(user);
 });
 
