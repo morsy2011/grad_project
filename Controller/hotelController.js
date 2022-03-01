fa2d880c1ce748aa511eaed9e57ec96736e10ee8
Mahmoud Morsy <mahmoudmorsy530@gmail.com>
2022-03-01 02:21:34 +0200

    first edit

--- Controller/hotelController.js
+++ Controller/hotelController.js
@@ -1,5 +1,5 @@
+const Joi = require("joi");
 const express = require('express');
-const Joi = require('Joi');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const { Hotel, validate } = require('../models/hotel');
--- Controller/touristController.js
+++ Controller/touristController.js
@@ -1,5 +1,5 @@
+const Joi = require("joi");
 const express = require('express');
-const Joi = require('Joi');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const { Tourist, validate } = require('../models/tourist-place');
